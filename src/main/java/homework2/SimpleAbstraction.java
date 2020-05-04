ef42fe03f6232922ab9d52425b29c57199f7b904
NaMironova <NaMironova@yotateam.com>
2020-05-04 10:56:35 +0300

    Fix homework2; arrayList, sum and sort methods were added

--- src/main/java/homework2/SimpleAbstraction.java
+++ src/main/java/homework2/SimpleAbstraction.java
@@ -2,8 +2,15 @@ package homework2;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import static homework1.SimpleCalculator.getDouble;
 
 public class SimpleAbstraction {
+
     public static void main(String args[]) throws Exception {
 
         // Вывод 2 символов после запятой
@@ -18,6 +25,17 @@ public class SimpleAbstraction {
         Cube cube = new Cube(6.0);
         Prism prism = new Prism(3.4,12);
         Parallelepiped pal = new Parallelepiped(4.6, 12);
+
+        // Создаем набор фигур
+        ArrayList<Figure> suite = new ArrayList<>();
+        suite.add(ball);
+        suite.add(cylyinder);
+        suite.add(konus);
+        suite.add(pyramid);
+        suite.add(cube);
+        suite.add(prism);
+        suite.add(pal);
+        Collections.sort(suite);
         System.out.println("Объем шара: "+f.format(ball.getVolume()));
         System.out.println("Объем цилиндра: "+f.format(cylyinder.getVolume()));
         System.out.println("Объем конуса: "+f.format(konus.getVolume()));
@@ -25,7 +43,35 @@ public class SimpleAbstraction {
         System.out.println("Объем призмы:" +f.format(prism.getVolume()));
         System.out.println("Объем куба:" +f.format(cube.getVolume()));
         System.out.println("Объем параллелепипеда:" +f.format(pal.getVolume()));
-    }
 
+        //Сортировка фигур по объёму по возрастанию
+        System.out.println("Сортировка фигур по объёму:");
+        for(Figure fig: suite){
+            System.out.println(f.format(fig.getVolume()));
+        }
+
+        // Сумма объёмов всех фигур
+        double sum = 0;
+        for (Figure fig : suite) {
+            sum=sum+fig.getVolume();
+        }
+        System.out.println("Сумма объёмов всех фигур: "+f.format(sum));
+
+        // Поиск элемента по заданным параметрам
+        // Ищем объект ball
+        Ball compareBall = new Ball(4.5);
+        if (suite.contains(compareBall)) {
+            System.out.println("Фигура с заданными параметрами присутствует в наборе"+compareBall);
+        } else {
+            System.out.println("Фигуры с таким параметром в наборе нет"+compareBall);
+        }
+
+        if(suite.contains(ball)) {
+            System.out.println("Фигура с заданными параметрами присутствует в наборе" + ball);
+        } else {
+            System.out.println("Фигуры с таким параметром в наборе нет"+ball);
+        }
+
+    }
 }
 
